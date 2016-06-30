729f651c228cdd7a071fdcabcc1bdb78cac39620
Andy Phillips <phillips.andrewj@gmail.com>
2016-06-30 23:48:16 +0100

    Created MySQL DB called myapp, with a user to access it

--- modules/profiles/manifests/mysql.pp
+++ modules/profiles/manifests/mysql.pp
@@ -1,3 +1,10 @@
 class profiles::mysql {
   include ::mysql::server
+
+  mysql::db { 'myapp':
+    user     => 'myuser',
+    password => 'mypass',
+    host     => 'localhost',
+    grant    => ['SELECT', 'UPDATE'],
+  }
 }
