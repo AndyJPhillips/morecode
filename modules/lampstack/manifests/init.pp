83ce4c96c59841662f01973618aef3edbfd6867c
Andy Phillips <phillips.andrewj@gmail.com>
2016-06-30 22:08:18 +0100

    Updating manifest for creating myapp user

--- modules/lampstack/manifests/init.pp
+++ modules/lampstack/manifests/init.pp
@@ -1,7 +1,13 @@
 class lampstack {
   user { 'myapp':
     ensure => present,
-    password => "$1$ui.Xf.ZN$Z1AgeueqVKSZx5EK2auvA0",
+    password => "$6$saltsalt$t6Sww7xOSuip49dTO/RgRxQPbz0kisV1S6M2LfQzIKHUJFk4sHJTacAGek7KWOBYPp1WVeZmsKWzykEmiwJCI.",
+    home => "/home/myapp",
+  }
+  file { '/home/myapp':
+    ensure => directory,
+    owner => 'myapp',
+    require => User['myapp'],
   }
 }
 
