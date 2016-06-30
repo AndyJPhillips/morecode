e1e4d039691ffeee0d5912bb5e58db96781831e2
Andy Phillips <phillips.andrewj@gmail.com>
2016-06-30 23:32:55 +0100

    Got the phpinfo page working

--- modules/profiles/manifests/webserver.pp
+++ modules/profiles/manifests/webserver.pp
@@ -1,4 +1,10 @@
 class profiles::webserver {
   include apache
   include apache::mod::php
+
+  file {'/var/www/html/phpinfo.php':
+    ensure => file,
+    source => 'puppet:///modules/php/phpinfo.php',
+    mode => '0644',
+  }
 }
