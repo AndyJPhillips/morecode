496c80d3d70a302de8615fd4a2b4f2186d03af32
Andy Phillips <phillips.andrewj@gmail.com>
2016-06-30 21:19:54 +0100

    Initial commit of Puppet code

---
+++ manifests/site.pp
@@ -0,0 +1,13 @@
+node 'puppetmaster.local' {
+  class { 'puppetdb::globals':
+    version => '2.3.7-1.el7',
+  }
+  class { 'puppetdb':
+    listen_address => '0.0.0.0',
+  }
+  class { 'puppetdb::master::config':}
+}
+
+node 'puppetagent.local' {
+
+}
