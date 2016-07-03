1826a7a2f9fc0fa9d921dcf9367a2bf888538c19
Andy Phillips <phillips.andrewj@gmail.com>
2016-07-03 20:15:43 +0100

    Got Hiera working - typo in /etc/puppet/hiera.yaml

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,3 @@
-node 'puppetmaster.local' {
-  class { 'puppetdb::globals':
-    version => '2.3.7-1.el7',
-  }
-  class { 'puppetdb':
-    listen_address => '0.0.0.0',
-  }
-  class { 'puppetdb::master::config':}
-}
-
-node 'puppetagent.local' {
-  include roles::lampstack
+node default {
+  hiera_include('classes')
 }
